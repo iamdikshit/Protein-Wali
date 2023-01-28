20fc01fad90d1366d585963d83e00f3569c7dd3f
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-01-28 09:07:55 +0530

    Added auth Middleware in Middlewares
    Co-Authored-By: Dikshit Bhardwaj <54199681+iamdikshit@users.noreply.github.com>

---
+++ middlewares/auth.Middleware.js
@@ -0,0 +1,42 @@
+import User from "../models/user.Schema.js";
+import JWT from "jsonwebtoken";
+import asyncHandler from "../services/asyncHandler.js";
+import AppError from "../services/appError.js";
+import config from "../config/env.config.js";
+
+
+export const isSignnedIn = asyncHandler(async (req, _res, next) => {
+       
+       // Declaring token so that it could be used in whole block
+       let token;
+
+       // Checking token in Cookies & in Bearer Token
+       if (req.cookies.token || 
+          (req.headers.Authorization && req.headers.Authorization.startsWith("Bearer")))
+       {
+            // Fetching Token Value from either Cookies or From Bearer Token and Storing in "token".
+            token = req.cookies.token || req.headers.Authorization.split(" ")[1];
+       };
+
+       // Checking Whether token (tokenValue) is present or not
+       if(!token) {
+            throw new AppError("Not Authorized to Access this Route.",401);
+       };
+
+
+       try {
+
+        // Verifying Token
+        const decodedJwtPayload = JWT.verify(token, config.JWT_SECRET);
+
+        // Finding User based on "_id" and with Selected Fields (Name, Email, Role) and Sending it to req.user
+        req.user = await User.findById(decodedJwtPayload._id, "name email role");
+
+        next();
+
+       } 
+       catch (error) {
+            throw new AppError("Not Authorized to Access this Route.",401);
+       };
+
+});
