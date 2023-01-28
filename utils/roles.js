de42bcf85b20b89bb98d5a16f0e2e28ba18389cd
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-01-28 09:24:39 +0530

    Refactor Roles
    Co-Authored-By: Dikshit Bhardwaj <54199681+iamdikshit@users.noreply.github.com>

--- utils/roles.js
+++ utils/roles.js
@@ -1,9 +1,9 @@
-const AuthRoles = Object.freeze({
+const Roles = Object.freeze({
     ADMIN : "ADMIN",
     MODERATOR : "MODERATOR",
     USER : "USER",
 });
 
-export default AuthRoles;
+export default Roles;
 
 // Enum Created For Roles
