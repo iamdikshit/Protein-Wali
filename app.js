ecb524eed05c47f662330cdc83c27c9c8f32089a
Anirban Middya <anirbanmiddya0011@gmail.com>
2023-01-28 09:59:56 +0530

    Added User Route in Routes
    Co-Authored-By: Dikshit Bhardwaj <54199681+iamdikshit@users.noreply.github.com>

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import express from "express";
 import morgan from "morgan";
 import config from "./config/env.config.js"
-import userRoute from "./routes/user.Route.js";
+import UserRoute from "./routes/user.Route.js";
 import AppError from "./utils/appError.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
@@ -21,9 +21,18 @@ if (config.NODE_ENV.trim() == "development") {
   app.use(morgan("dev"));
 }
 
-
 // API ENDPOINT FOR USER
-app.use("/api/v1/user",userRoute);
+
+/******************************************************
+ * @AUTH_ROUTES
+ * @Route_1   /api/v1/user/signup
+ * @Route_2   /api/v1/user/signin
+ * @Route_3   /api/v1/user/signout
+ * @Route_4   /api/v1/user/password/forgot
+ * @Route_5   /api/v1/user/password/:resetToken
+ * @Route_6   /api/v1/user/password/change
+ ******************************************************/
+app.use("/api/v1/user",UserRoute);
 
 
 
--- routes/user.Route.js
+++ routes/user.Route.js
@@ -1,7 +1,27 @@
 import express from 'express'
-import {getAllUser} from '../controllers/userController.js'
+import { signUp, signIn, signOut, forgotPassword, resetPassword, changePassword } from '../controllers/auth.Controller.js'
 const Router = express.Router();
 
-Router.get('/',getAllUser);
+/******************************************************
+ * @AUTH_ROUTES
+ * @Route_1   /signup
+ * @Route_2   /signin
+ * @Route_3   /signout
+ * @Route_4   /password/forgot
+ * @Route_5   /password/:resetToken
+ * @Route_6   /password/change
+ ******************************************************/
+
+Router.get('/signup',signUp);
+Router.get('/signin',signIn);
+Router.get('/signout',signOut);
+Router.get('/password/forgot',forgotPassword);
+Router.get('/password/:resetToken',resetPassword);
+Router.get('/password/change',changePassword);
+
+
+
+
+
 
 export default Router;
