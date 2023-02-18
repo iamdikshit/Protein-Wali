534fc7c343ad9796797071998f2eeb76ae110c80
iamdikshit <dikshit.b.122@gmail.com>
2023-02-18 16:20:23 +0530

    Changes in product schema

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -1,21 +1,20 @@
-import asyncHandler from "../services/asyncHandler.js";
-import AppError from "../services/appError.js";
 import Product from "../models/product.Schema.js";
+import asyncHandler from "../services/asyncHandler.js";
 
-
-
-
-
-
-
-
-
-export const insertProduct = asyncHandler(async (req, res, next) => {
-    console.log(req.body);
-    const category = await Product.create(req.body);
-  
-    res.status(201).json({
-      status: "success",
-      data: category,
-    });
+/******************************************************
+ * @GET_ALL_PRODUCTS
+ * @REQUEST_TYPE GET
+ * @Route       /
+ * @Description Controller to fetch all the products from the database
+ * @Middleware
+ * @Parameters
+ * @Returns Product Object
+ ******************************************************/
+export const getAllProducts = asyncHandler(async (req, res, next) => {
+  const products = await Product.find();
+  return res.status(200).json({
+    status: "success",
+    results: products.length,
+    data: products,
   });
+});
